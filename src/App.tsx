14f33eb44b23c6dc81408956e3fd275876f5cb3c
SethPython <spccmbseth@gmail.com>
2025-02-19 21:07:55 +0530

    main home page

--- src/App.tsx
+++ src/App.tsx
@@ -2,9 +2,26 @@ import './index.css'
 
 function App() {
   return (
-    <>
-      <h1 className='text-center text-7xl'>Hello World</h1>
-    </>
+    <main className='bg-offwhite font-inter h-lvh'>
+      <div className='flex flex-col gap-4 py-20'>
+        <div className='flex flex-row'>
+          <h1 className='mx-8 text-8xl text-blue font-extrabold'>THINK</h1>
+          <div className='w-full bg-gold' />
+        </div>
+        <div className='flex flex-row'>
+          <div className='w-full bg-gold' />
+          <h1 className='mx-8 text-8xl text-blue font-extrabold'>CREATE</h1>
+        </div>
+        <div className='flex flex-row'>
+          <div className='w-1/6 bg-gold' />
+          <h1 className='mx-8 text-8xl text-blue font-extrabold'>INNOVATE</h1>
+          <div className='w-full bg-gold' />  
+        </div>
+      </div>
+      <div className='py-6 px-10'>
+        <p className='text-2xl font-normal'>LOREM IPSUM ODOR AMET, CONSECTETUER ADIPISCING ELIT. ULTRICES LOBORTIS SUSCIPIT SUSPENDISSE NISL ETIAM CURABITUR. CONSEQUAT CONSEQUAT PROIN ELEMENTUM PORTTITOR NON. COMMODO LIBERO TORTOR EU IPSUM VARIUS ULTRICIES VARIUS AD CURAE. SOCIOSQU TEMPOR CONDIMENTUM IACULIS; FINIBUS NASCETUR PULVINAR ENIM SODALES.</p>
+      </div>
+    </main>
   )
 }
 
--- tailwind.config.js
+++ tailwind.config.js
@@ -5,8 +5,19 @@ export default {
   content: ["./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
     extend: {},
+    colors: {
+      'blue': '#102542',
+      'offwhite': '#f4f1e6',
+      'gold': '#bf8812'
+    },
+    fontFamily: {
+      'inter': ['Inter']
+    }
   },
   plugins: [
     daisyui
   ],
+  daisyui: {
+    themes: []
+  }
 }
