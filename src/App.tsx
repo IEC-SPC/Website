4ee74816d9dcad3e1f759ec77be92c4153a34c86
SethPython <spccmbseth@gmail.com>
2025-02-21 18:19:31 +0530

    Use section tags and add smooth scroll

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,7 @@ import './index.css'
 function App() {
   return (
     <main className='bg-offwhite font-inter h-lvh'>
-      <div className='flex flex-row p-10'>
+      <section className='flex flex-row p-10'>
         <img src='/iec_logo.png' width={173} height={42} />
         <div className='w-full' />
         <div className='flex flex-row gap-10 text-nowrap'>
@@ -12,8 +12,9 @@ function App() {
           <a href='#'>Gallery</a>
           <a href='#'>Contact</a>
         </div>
-      </div>
-      <div className='flex flex-col gap-4 py-20'>
+      </section>
+
+      <section className='flex flex-col gap-4 py-20'>
         <div className='flex flex-row'>
           <h1 className='mx-8 text-8xl text-blue font-extrabold'>THINK</h1>
           <div className='w-full bg-gold' />
@@ -27,10 +28,11 @@ function App() {
           <h1 className='mx-8 text-8xl text-blue font-extrabold'>INNOVATE</h1>
           <div className='w-full bg-gold' />  
         </div>
-      </div>
-      <div className='py-6 px-10'>
+      </section>
+      
+      <section className='py-6 px-10'>
         <p className='text-2xl font-normal'>LOREM IPSUM ODOR AMET, CONSECTETUER ADIPISCING ELIT. ULTRICES LOBORTIS SUSCIPIT SUSPENDISSE NISL ETIAM CURABITUR. CONSEQUAT CONSEQUAT PROIN ELEMENTUM PORTTITOR NON. COMMODO LIBERO TORTOR EU IPSUM VARIUS ULTRICIES VARIUS AD CURAE. SOCIOSQU TEMPOR CONDIMENTUM IACULIS; FINIBUS NASCETUR PULVINAR ENIM SODALES.</p>
-      </div>
+      </section>
     </main>
   )
 }
